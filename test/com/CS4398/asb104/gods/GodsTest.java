448fa76023f65b5977eaaa613e7f4160ece4516d
Asb104 <asb104@txstate.edu>
2020-01-27 21:10:34 -0600

    Testing JUnit Environment for Project added

---
+++ test/com/CS4398/asb104/gods/GodsTest.java
@@ -0,0 +1,23 @@
+package com.CS4398.asb104.gods;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.CS4398.spc51.gods.Gods;
+
+
+public class GodsTest {
+
+	@Before
+	public void setup() {
+		Gods gods = new Gods();  //Local variable to be used for testing Class Gods
+	}
+	
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
