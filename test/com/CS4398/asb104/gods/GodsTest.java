dabda101cfebff7b7c0a2992b26e2ae9d2b4657d
Asb104 <asb104@txstate.edu>
2020-02-14 13:50:52 -0600

    Added to pom and test

--- test/com/CS4398/asb104/gods/GodsTest.java
+++ test/com/CS4398/asb104/gods/GodsTest.java
@@ -9,21 +9,24 @@ import org.bukkit.plugin.java.JavaPlugin;
 import com.CS4398.spc51.gods.Gods;
 
 
-/*
- * Notes for future tests:
- * 
- * Need to check various testing abilities.
- * 
- * Simulate a player join
- * simulate a player death
- * simulate a player kills mob
- * simulate a player breaks block ////this might be hard
- * simulate a player places a block ///this might be hard
- * simulate a player chat event
- * simulate a player command event
- * 
- */
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.never;
 
+import org.bukkit.entity.Player;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PowerMockIgnore;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+@PowerMockIgnore("org.mockito.*")
+//@RunWith(PowerMockRunner.class)
+@PrepareForTest(PlayerJoinEvent.class)
 public class GodsTest {
 
 	@Before
@@ -32,10 +35,8 @@ public class GodsTest {
 	}
 	
 	@Test
-	public void VariableCreation() {
-		Gods testGod = new Gods();
-		Gods gods = new Gods();
-		assertEquals(testGod, gods);
-	}
+    public void testOnPlayerJoin() {
+        
+   }
 
 }
