3c3f735670590fe63471d94a15aa3abdbf2d6d91
spc51 <spc51@txstate.edu>
2020-02-20 17:48:08 -0600

    Added Atheist god. This is the default god.

---
+++ src/com/CS4398/spc51/gods/gods/Atheist.java
@@ -0,0 +1,26 @@
+package com.CS4398.spc51.gods.gods;
+
+import org.bukkit.event.Event;
+
+import com.CS4398.spc51.gods.Believer;
+
+public class Atheist extends God{
+
+	public Atheist(String name, String religion) {
+		super(name, religion);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void observe(Believer believer, Event e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void reward(float previousBP, float currentBP) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
