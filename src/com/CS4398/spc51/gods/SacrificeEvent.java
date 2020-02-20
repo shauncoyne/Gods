5e57b8e7e28d3b998d8f64ffcd99c21db1d14a2b
spc51 <spc51@txstate.edu>
2020-02-20 17:48:32 -0600

    Added a custom event for when a player sacrifices an entity on an alter

---
+++ src/com/CS4398/spc51/gods/SacrificeEvent.java
@@ -0,0 +1,49 @@
+package com.CS4398.spc51.gods;
+
+import org.bukkit.event.Cancellable;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+
+import com.CS4398.spc51.gods.alter.Alter;
+
+public class SacrificeEvent extends Event implements Cancellable {
+    private Alter alter;
+    private EntityDamageByEntityEvent e;
+    private static final HandlerList HANDLERS_LIST = new HandlerList();
+    private boolean isCancelled;
+
+    public SacrificeEvent(EntityDamageByEntityEvent e, Alter alter){
+        this.e = e;
+        this.alter = alter;
+        this.isCancelled = false;
+    }
+
+    @Override
+    public boolean isCancelled() {
+        return isCancelled;
+    }
+
+    @Override
+    public void setCancelled(boolean cancelled) {
+        this.isCancelled = cancelled;
+    }
+
+    @Override
+    public HandlerList getHandlers() {
+        return HANDLERS_LIST;
+    }
+
+    public static HandlerList getHandlerList() {
+        return HANDLERS_LIST;
+    }
+
+    public EntityDamageByEntityEvent getEntityDamageByEntityEvent() {
+        return e;
+    }
+    public Alter getAlter() {
+    	return alter;
+    }
+
+
+}
