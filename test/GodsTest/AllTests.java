90f10ba405d79d23399defdf35ee195de7eafde8
Asb104 <asb104@txstate.edu>
2020-03-02 14:29:52 -0600

    Refactoring code for organization

--- test/GodsTest/AllTests.java
+++ test/GodsTest/AllTests.java
@@ -1,10 +1,11 @@
-package test;
+package GodsTest;
 
 import static org.junit.Assert.*;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.bukkit.plugin.java.JavaPlugin;
+import com.CS4398.spc51.gods.*;
 
 import com.CS4398.spc51.gods.Gods;
 
@@ -13,30 +14,28 @@ import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+
+import java.util.UUID;
+
 import static org.mockito.Mockito.never;
 
 import org.bukkit.entity.Player;
 import org.bukkit.event.player.PlayerJoinEvent;
-import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.powermock.api.mockito.PowerMockito;
-import org.powermock.core.classloader.annotations.PowerMockIgnore;
-import org.powermock.core.classloader.annotations.PrepareForTest;
-import org.powermock.modules.junit4.PowerMockRunner;
 
-@PowerMockIgnore("org.mockito.*")
-//@RunWith(PowerMockRunner.class)
-@PrepareForTest(PlayerJoinEvent.class)
-public class test {
 
+
+public class AllTests  {
+
+	
 	@Before
 	public void setup() {
 		
 	}
 	
 	@Test
-    public void testOnPlayerJoin() throws Exception {
+    public void testOnPlayerJoin() {
         
    }
-
+//	
 }
---
+++ test/test/test.java
@@ -0,0 +1,35 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import com.CS4398.spc51.gods.Gods;
+
+
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.never;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.junit.runner.RunWith;
+
+
+public class test {
+
+	@Before
+	public void setup() {
+		
+	}
+	
+	@Test
+    public void testOnPlayerJoin() throws Exception {
+        
+   }
+
+}
