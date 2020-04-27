a9dda0bad831bf740826072ee683144d9810d381
nrunas <nrunas@users.noreply.github.com>
2020-04-27 18:11:46 -0500

    added cases to powerups, still not populated

--- src/com/CS4398/spc51/gods/gods/Hades.java
+++ src/com/CS4398/spc51/gods/gods/Hades.java
@@ -152,6 +152,25 @@ public class Hades extends God{
 		{
 		case 1:
 			break;
+		case 2:
+			break;
+		case 3:
+			break;
+		case 4:
+			break;
+		case 5:
+			break;
+		case 6:
+			break;
+		case 7:
+			break;
+		case 8:
+			break;
+		case 9:
+			break;
+		case 10:
+			break;
+		
 			default: break;
 		}
 		return thing;
--- src/com/CS4398/spc51/gods/gods/Poseidon.java
+++ src/com/CS4398/spc51/gods/gods/Poseidon.java
@@ -172,6 +172,24 @@ public class Poseidon extends God{
 		{
 		case 1:
 			break;
+		case 2:
+			break;
+		case 3:
+			break;
+		case 4:
+			break;
+		case 5:
+			break;
+		case 6:
+			break;
+		case 7:
+			break;
+		case 8:
+			break;
+		case 9:
+			break;
+		case 10:
+			break;
 			default: break;
 		}
 		return thing;
